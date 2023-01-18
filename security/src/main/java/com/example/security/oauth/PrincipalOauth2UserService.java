1b936d5b200afe66f4bd5e45212d7ac9eb05db21
bshello25 <bshello25@gmail.com>
2023-01-19 07:46:20 +0900

    230119

--- security/src/main/java/com/example/security/oauth/PrincipalOauth2UserService.java
+++ security/src/main/java/com/example/security/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,8 @@ package com.example.security.oauth;
 import com.example.security.auth.PrincipalDetails;
 import com.example.security.model.User;
 import com.example.security.oauth.provider.GoogleUserInfo;
+import com.example.security.oauth.provider.KakaoUserInfo;
+import com.example.security.oauth.provider.NaverUserInfo;
 import com.example.security.oauth.provider.OAuth2UserInfo;
 import com.example.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -21,26 +25,34 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     //구글로부터 받은 userRequest데이터에 대해 후처리되는함수
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        System.out.println("getAccessToken:" + userRequest.getAccessToken());
-        System.out.println("getClientRegistration:" + userRequest.getClientRegistration());
-        System.out.println("getRegistrationId:" + userRequest.getClientRegistration().getRegistrationId());
-        System.out.println("getClientRegistration.getClientId:" + userRequest.getClientRegistration().getClientId());
+//        System.out.println("getAccessToken:" + userRequest.getAccessToken());
+//        System.out.println("getClientRegistration:" + userRequest.getClientRegistration());
+//        System.out.println("getRegistrationId:" + userRequest.getClientRegistration().getRegistrationId());
+//        System.out.println("getClientRegistration.getClientId:" + userRequest.getClientRegistration().getClientId());
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        System.out.println("getAttributes:" + oAuth2User.getAttributes());
-
+//        System.out.println("getAttributes:" + oAuth2User.getAttributes());
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         OAuth2UserInfo oAuth2UserInfo = null;
         if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("google");
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
-        }else{
-
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("naver");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+            System.out.println("kakao");
+            oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes());
+        }else {
+            System.out.println("--------------");
         }
 
 //        String provider = userRequest.getClientRegistration().getRegistrationId();
 //        String providerId = oAuth2User.getAttribute("sub");
 //        String username = provider+"_"+providerId;
 //        String email = oAuth2User.getAttribute("email");
-
+        System.out.println("test----------------------------------------");
+        System.out.println(oAuth2UserInfo.getProviderId());
         String provider = oAuth2UserInfo.getProvider();
         String providerId = oAuth2UserInfo.getProviderId();
         String username = provider+"_"+providerId;
---
+++ security/src/main/java/com/example/security/oauth/provider/KakaoUserInfo.java
@@ -0,0 +1,37 @@
+package com.example.security.oauth.provider;
+
+import java.util.Map;
+
+public class KakaoUserInfo implements OAuth2UserInfo{
+
+    private Map<String, Object> attributes;
+    private Map<String, Object> attributesAccount;
+
+    public KakaoUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+        this.attributesAccount = (Map<String, Object>) attributes.get("kakao_account");
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getName() {
+        Map<String, Object> attributesProfile = (Map<String, Object>) attributesAccount.get("profile");
+        return attributesProfile.get("nickname").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attributesAccount.get("email").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+}
+
